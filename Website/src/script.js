b83eec52d40725df9fc38d5a2d268eb5b452fbd5
Ernest Spiller <e.spiller2002@gmail.com>
2024-01-30 20:48:01 -0800

    Organized data

---
+++ Website/src/script.js
@@ -0,0 +1,13 @@
+// Add any JavaScript functionality you need here
+// This file is optional, only if you need to add interactivity.
+
+// Example: Smooth scrolling to anchor links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
